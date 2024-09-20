49ef309446e534bb751017234ab27e1379d0c48b
Megha Bhutada <103251280+coder-megha242@users.noreply.github.com>
2024-09-20 09:51:35 +0000

    10th commit

---
+++ 02_Basics/objects.js
@@ -0,0 +1,20 @@
+
+//Object literals
+
+//object.create // This is construction method // It is also known as singleton
+
+const JsUser = {
+    name : "Hitesh",
+    fullName : "Hitesh houdhary",
+    age : 18 ,
+    location : Jaipur,
+    email : "abc@gmail.com",
+    isLoggedin : False,
+    LastLoginDay : ["Monday", "Tuesday", Saturday]
+
+}
+
+console.log(JsUser.name);
+console.log(JsUser.age);
+console.log(JsUser["email"]);
+console.log(JsUser["fullName"]);
